fc4eca80be125d9147334818b5a247cf6b56493e
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-01 16:31:56 -0700

    added theme, got image to be better size, title is bigger and colored

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React from 'react';
 // import Container from "@mui/material/Container";
-import { Grid, Paper, Typography, TextField, Button } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material";
-import logo from "../images/logo.png";
+import {
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Container,
+} from '@mui/material';
+import logo from '../images/logo.png';
 
 const style = {
-	img: {
-		width: "192px",
-		height: "192px",
-	},
+  img: {
+    width: '25rem',
+    height: 'auto',
+  },
 };
 
-const buttonTheme = createTheme({
-	palette: {
-		primary: {
-			main: "#116466",
-		},
-	},
-});
+// const buttonTheme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#116466',
+//     },
+//   },
+// });
 
 // const topTheme = createTheme({
 // 	palette: {
@@ -28,59 +34,72 @@ const buttonTheme = createTheme({
 // });
 
 function SignIn() {
-	return (
-		<Grid
-			container
-			direction="Column"
-			justifyContent="center"
-			alignItems="center"
-		>
-			<Grid item>
-				<img alt="stat-tasktic logo" style={style.img} src={logo} />
-			</Grid>
-			<Grid item>
-				<Typography>Stat-tasktic</Typography>
-			</Grid>
-			<Grid item xs={10}>
-				<Grid
-					container
-					item
-					direction="column"
-					justifyContent="center"
-					alignItems="center"
-				>
-					<Paper elevation={3}>
-						<Grid item margin={2}>
-							<Typography variant="h3" component="h1" textAlign={"center"}>
-								Sign In
-							</Typography>
-						</Grid>
-						<Grid item margin={2}>
-							<TextField
-								id="filled-basic"
-								label="Email"
-								variant="filled"
-								required="true"
-							/>
-						</Grid>
-						<Grid item margin={2}>
-							<TextField
-								id="filled-basic"
-								label="Password"
-								variant="filled"
-								required="true"
-							/>
-						</Grid>
-						<Grid item margin={2} align="center">
-							<ThemeProvider theme={buttonTheme}>
-								<Button variant="contained">Submit</Button>
-							</ThemeProvider>
-						</Grid>
-					</Paper>
-				</Grid>
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Container sx={{ bgcolor: 'primary.dark' }}>
+      <Grid
+        container
+        direction="column"
+        justifyContent={'center'}
+        alignItems="center"
+      >
+        <Grid item>
+          <img alt="stat-tasktic logo" style={style.img} src={logo} />
+        </Grid>
+        <Grid item>
+          <Typography variant="h1" sx={{ color: 'secondary.light' }}>
+            Stat-tasktic
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid
+        sx={{
+          bgcolor: 'primary.main',
+        }}
+        container
+        direction="Column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={10}>
+          <Grid
+            container
+            item
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Paper elevation={3}>
+              <Grid item margin={2}>
+                <Typography variant="h3" component="h1" textAlign={'center'}>
+                  Sign In
+                </Typography>
+              </Grid>
+              <Grid item margin={2}>
+                <TextField
+                  id="filled-basic"
+                  label="Email"
+                  variant="filled"
+                  required="true"
+                />
+              </Grid>
+              <Grid item margin={2}>
+                <TextField
+                  id="filled-basic"
+                  label="Password"
+                  variant="filled"
+                  required="true"
+                />
+              </Grid>
+              <Grid item margin={2} align="center">
+                <Button variant="contained">Submit</Button>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default SignIn;
