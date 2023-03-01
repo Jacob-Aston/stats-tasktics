ef03399d6d6fc67fd69820d91fea34eb87a61f42
Brent Keller <bikeller104@gmail.com>
2023-02-28 19:39:00 -0700

    added initial type definition for graphql with users task and list types defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,17 @@
 const { gql } = require('apollo-server-express');
 
+
+/*
+Type defs are used by the graph ql client and define what queries the cliend is allowed to make    type User {
+        _id: ID <-- required id of type ID 
+        username: String <-- the user name property in the user model (its a string) 
+        the : seperates type from name
+        email: String
+        password: String
+        lists: [List] <-- our custom type that is defined under the user type, cant use this without defining it in the typedef
+    }
+
+*/
 const typeDefs = gql`
     type User {
         _id: ID
@@ -11,17 +23,24 @@ const typeDefs = gql`
 
     type List {
         _id: ID
-        taskRefreshDay
-        task
+        taskRefreshDay: String
+        task: Task
     }
 
     type Task {
-        title
+        _id: ID
+        title: String
     }
+
+
 
 
     type Query {
-        me: [User]
+        users: [User]
+        user(userId: ID!): User
+        me: User
         lists(email: String): [List]
     }
 `
+
+module.exports =  typeDefs;
