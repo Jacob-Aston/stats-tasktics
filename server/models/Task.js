30137902af78111c5ac36c56c39ca20e71f794f1
Brent Keller <bikeller104@gmail.com>
2023-02-28 18:26:54 -0700

    added a task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,5 +1,39 @@
 const { Schema, model } = require("mongoose");
 
 const taskSchema = new Schema({
-    
-})
+    title: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    description: {
+        type: String,
+        required: false,
+        unique: false,
+        /*
+        javascript has a get day function that returns a number 0 - 6 for the day of the week of the current date
+        */
+    },
+    dueDate: {
+        type: String,
+        required: true,
+        unique: false,
+        trim: true,
+        match:  [/(Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday)/, 'Must match a day of the week!']
+    },
+    startTime: {
+        type: Date,
+        required: false,
+        unique: false,
+    },
+    finishTime: {
+        type: Date,
+        required: false,
+        unique: false,
+    }
+});
+
+//this function gives the user schema its name!!!
+const TaskSchema = model('Task', taskSchema);
+
+module.exports = TaskSchema;
