ed3addae82094df8ac16d38ce365320172d5ae70
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-01 16:05:53 -0700

    added theme to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#116466',
+      dark: '#2C3531',
+      light: '#D9B08C',
+    },
+    secondary: {
+      main: '#FFCB9A',
+      light: '#D1E8E2',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
