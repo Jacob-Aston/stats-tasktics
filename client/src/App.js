fef2c39d568f92685c23978167c41dd0e66490bf
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-02 19:18:27 -0700

    added tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,98 +1,94 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import {
-	ApolloClient,
-	InMemoryCache,
-	ApolloProvider,
-	createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 
 // import { createTheme, ThemeProvider } from "@mui/material";
 
 // import Home from './pages/Home';
 // import Signup from './pages/Signup';
-import SignIn from "./pages/SignIn";
+import SignIn from './pages/SignIn';
 // import SingleThought from './pages/SingleThought';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+import Stats from './pages/Stats';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-	uri: "/graphql",
+  uri: '/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-	// get the authentication token from local storage if it exists
-	const token = localStorage.getItem("id_token");
-	// return the headers to the context so httpLink can read them
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : "",
-		},
-	};
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
-	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-	link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 const theme = createTheme({
-	typography: {
-		allVariants: {
-			color: "#2C3531",
-		},
-	},
-	palette: {
-		// mode: "dark",
-		background: {
-			default: "#116466",
-		},
-		default: {
-			gray: "#2C3531",
-			darkBlue: "#116466",
-			blue: "#D1E8E2",
-			darkTan: "#D9B08C",
-			tan: "#FFCD9A",
-		},
-	},
+  typography: {
+    allVariants: {
+      color: '#2C3531',
+    },
+  },
+  palette: {
+    // mode: "dark",
+    background: {
+      default: '#116466',
+    },
+    default: {
+      gray: '#2C3531',
+      darkBlue: '#116466',
+      blue: '#D1E8E2',
+      darkTan: '#D9B08C',
+      tan: '#FFCD9A',
+    },
+  },
 });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<Router>
-					<div className="flex-column justify-flex-start min-100-vh">
-						<div className="container">
-							<Routes>
-								{/* <Route 
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <div className="flex-column justify-flex-start min-100-vh">
+            <div className="container">
+              <Routes>
+                {/* <Route 
                 path="/" 
                 element={<Home />} 
               /> */}
-								<Route path="/signin" element={<SignIn />} />
-								{/* <Route 
+                <Route path="/signin" element={<SignIn />} />
+                {/* <Route 
                 path="/signup" 
                 element={<Signup />} 
               /> */}
-								{/* <Route 
-                path="/thoughts/:thoughtId" 
-                element={<SingleThought />} 
-              /> */}
-							</Routes>
-						</div>
-					</div>
-				</Router>
-			</ThemeProvider>
-		</ApolloProvider>
-	);
+                <Route path="/stats" element={<Stats />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Stats.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  Grid,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Link,
+  Tabs,
+  Tab,
+} from '@mui/material';
+import logo from '../images/statslogoph.png';
+
+const styles = {
+  img: {
+    width: '95px',
+    height: '145px',
+    paddingTop: '20px',
+  },
+};
+
+const handleChange = function () {};
+
+function Stats() {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item>
+        <img alt="stat-tasktic logo" style={styles.img} src={logo} />
+      </Grid>
+      <Grid item>
+        <Typography variant="h3" component="h1" color="black">
+          Stat-tasktic
+        </Typography>
+      </Grid>
+      <Grid item xs={1} marginY={4} marginX={1}>
+        <Paper elevation={7} sx={{ backgroundColor: 'default.tan' }}>
+          <Grid
+            container
+            item
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {/* This is the tabs for Stats and Tasks  */}
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value="Tabs" onChange={handleChange} variant="fullWidth">
+                <Tab label="Stats" />
+                <Tab label="Tasks" />
+              </Tabs>
+            </Box>
+            <Box margin={1} paddingY={2}>
+              <Typography variant="h4" component="h2" textAlign={'center'}>
+                Sign In
+              </Typography>
+            </Box>
+            <Box margin={3}>
+              <TextField
+                id="filled-basic"
+                label="Email"
+                variant="filled"
+                required="true"
+                color="info"
+                sx={{ backgroundColor: 'default.blue' }}
+              />
+            </Box>
+            <Box margin={1}>
+              <TextField
+                id="filled-basic"
+                label="Password"
+                variant="filled"
+                required="true"
+                color="info"
+                align="center"
+                sx={{ backgroundColor: 'default.blue' }}
+              />
+            </Box>
+            <Box marginY={3} paddingTop={2} align="center">
+              <Button
+                variant="contained"
+                sx={{ backgroundColor: 'default.gray', color: 'default.blue' }}
+              >
+                Submit
+              </Button>
+            </Box>
+            <Box padding={2} marginBottom={2} textAlign="center">
+              <Link href="#" underline="hover">
+                {"Don't have an account? Sign up here"}
+              </Link>
+            </Box>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Stats;
