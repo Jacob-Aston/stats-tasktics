fdf205731e591f946872e3e97fb4eb0f54d1a9b1
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2023-03-06 14:19:14 -0700

    refactored bar graph code

--- client/src/charts/Bar.js
+++ client/src/charts/Bar.js
@@ -1,11 +1,19 @@
 import { Bar } from 'react-chartjs-2';
 
 const data = {
-  labels: ['On-Time', 'Overdue'],
+  // this is the label at the bottom of each column
+  labels: ['Last Week', 'This Week'],
   datasets: [
     {
-      data: [4, 6],
-      backgroundColor: ['Green', 'Red'],
+      // each dataset is a bar inside each of the above labels
+      label: 'On-Time',
+      data: '4',
+      backgroundColor: '#2C3531',
+    },
+    {
+      label: 'Complete',
+      data: '6',
+      backgroundColor: '#D1E8E2',
     },
   ],
 };
